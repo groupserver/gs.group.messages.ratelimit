231417a84c7614b8f21d3d1a21f8d72fb207d9f7
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-26 12:00:38 -0700

    Turning off zip_safe, and updating the egg deps.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -29,16 +42,21 @@ setup(name='gs.group.messages.ratelimit',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     install_requires=[
         'setuptools',
+        'zope.browserpage',
         'zope.cachedescriptors',
         'zope.component',
         'zope.formlib',
         'zope.interface',
         'zope.schema',
+        'zope.tal',
+        'zope.tales',
+        'zope.viewlet',
         'Zope2',
         'gs.content.form',
+        'gs.content.layout',
         'gs.group.base',
         'gs.group.member.viewlet',
         'gs.group.properties',
